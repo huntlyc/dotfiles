c3aaa84bcf45336e38580a7250624903b7755d14
Huntly <huntly@strutdigital.co.uk>
2022-09-26 13:40:01 +0100

    better copy/pasta

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -19,6 +19,18 @@ vim.keymap.set('n', '<leader>q', '<cmd>q<CR>')
 vim.keymap.set('n', '<leader>c', '<cmd>clo<CR>')
 vim.keymap.set('n', '<leader>bn', '<cmd>bnext<CR>')
 vim.keymap.set('n', '<leader>bp', '<cmd>bprevious<CR>')
+vim.keymap.set('n', "<leader>y", "\"+y")
+vim.keymap.set('v', "<leader>y", "\"+y")
+vim.keymap.set('n', "<leader>Y", "\"+Y")
+vim.keymap.set('n', "Y", "yg$")
+vim.keymap.set('n', "n", "nzzzv")
+vim.keymap.set('n', "N", "Nzzzv")
+vim.keymap.set('n', "J", "mzJ`z")
+vim.keymap.set('n', "<C-d>", "<C-d>zz")
+vim.keymap.set('n', "<C-u>", "<C-u>zz")
+
+vim.keymap.set('v', "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', "K", ":m '<-2<CR>gv=gv")
 
 -- packer
 vim.keymap.set('n', '<leader>ps', '<cmd>PackerSync<CR>')
